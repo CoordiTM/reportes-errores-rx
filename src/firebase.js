a9d1bf641f95a2d1502fc5cbf0f7c3303725fc60
CoordiTM <coordinaciontm.sabogal@gmail.com>
2026-06-28 17:13:20 -0500

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -2,12 +2,12 @@ import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyB4B9B7SzxeWC7h3tYXOu5TL0nGbbBH0rQ",
-  authDomain: "sistema-de-produccion-tm.firebaseapp.com",
-  projectId: "sistema-de-produccion-tm",
-  storageBucket: "sistema-de-produccion-tm.firebasestorage.app",
-  messagingSenderId: "357709452276",
-  appId: "1:357709452276:web:f3bdf0aa8f7faa63409fa1"
+  apiKey: "AIzaSyA9tUi7GALBDWvZA20Fz7P1NNBW_cfbxdk",
+  authDomain: "reporte-de-errores-rx.firebaseapp.com",
+  projectId: "reporte-de-errores-rx",
+  storageBucket: "reporte-de-errores-rx.firebasestorage.app",
+  messagingSenderId: "851330297843",
+  appId: "1:851330297843:web:0708a7c36867224679ac86"
 };
 
 const app = initializeApp(firebaseConfig);
